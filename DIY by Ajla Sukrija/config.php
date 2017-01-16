1316a63c8761e43f07a52c6cd5d356b023f80fa4
Ajla Sukrija <ajla.sukrija@gmail.com>
2017-01-16 21:46:23 +0100

    Openshift deployment problem fix #1

--- DIY by Ajla Sukrija/config.php
+++ DIY by Ajla Sukrija/config.php
@@ -11,4 +11,9 @@ $mysql_host = getenv('OPENSHIFT_MYSQL_DB_HOST');
 $mysql_user = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
 $mysql_pass  = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
 $mysql_db = getenv('OPENSHIFT_APP_NAME');
+
+echo "HOST: ".$mysql_host;
+echo "USER: ".$mysql_user;
+echo "PASS: ".$mysql_pass;
+echo "DB: ".$mysql_db;
 ?>
