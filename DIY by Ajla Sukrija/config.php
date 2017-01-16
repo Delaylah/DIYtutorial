324831e156a84bec9ed4a7c27c022c7196edef3d
Ajla Sukrija <ajla.sukrija@gmail.com>
2017-01-16 23:42:36 +0100

    Ispravka mysql podataka za openshifts

--- DIY by Ajla Sukrija/config.php
+++ DIY by Ajla Sukrija/config.php
@@ -1,15 +1,15 @@
 <?php
 // Local
-$mysql_host = "localhost";
+/*$mysql_host = "localhost";
 $mysql_user = "root";
 $mysql_pass = "";
-$mysql_db = "diy";
+$mysql_db = "diy";*/
 
 // Openshift
-/*$mysql_host = "10.1.101.5";
+$mysql_host = "10.1.101.5";
 $mysql_user = "diyuser";
 $mysql_pass  = "diy2017sa";
-$mysql_db = "diydb";*/
+$mysql_db = "diydb";
 
 function isUserLogedIn() {
     if (isset($_SESSION['username'])) {
