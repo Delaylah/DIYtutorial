8a0f1b0cee1b29062ebe2a92122a7faecf685523
Ajla Sukrija <ajla.sukrija@gmail.com>
2017-01-22 10:30:03 +0100

    Update IP for new MYSQL pod

--- DIY by Ajla Sukrija/config.php
+++ DIY by Ajla Sukrija/config.php
@@ -6,7 +6,7 @@ $mysql_pass = "";
 $mysql_db = "diy";*/
 
 // Openshift
-$mysql_host = "10.1.101.5";
+$mysql_host = "10.1.38.28";
 $mysql_user = "diyuser";
 $mysql_pass  = "diy2017sa";
 $mysql_db = "diydb";
